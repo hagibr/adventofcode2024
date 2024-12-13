e632ce2629426bc12a29e96ed3ab39af81ffbc74
Alexandre Hagihara <alexandre.hagihara@gmail.com>
2024-12-13 12:36:12 -0300

    Refactoring of part1 to not use a global variable

--- 11/part1.py
+++ 11/part1.py
@@ -5,27 +5,22 @@ input_file.close()
 
 initial = [int(v) for v in input_line.split()]
 
-# print(initial)
-
-count_stones = [0]
-
 def blink(blink_times, value):
   if blink_times == 25:
-    count_stones[0] = count_stones[0] + 1
-    return
+    return 1
   
   str_value = str(value)
   if value == 0:
-    blink(blink_times+1, 1)
+    return blink(blink_times+1, 1)
   elif len(str_value) % 2 == 0:
     left = int(str_value[:len(str_value)//2])
     right = int(str_value[len(str_value)//2:])
-    blink(blink_times+1, left)
-    blink(blink_times+1, right)
+    return blink(blink_times+1, left) + blink(blink_times+1, right)
   else:
-    blink(blink_times+1, value*2024)
+    return blink(blink_times+1, value*2024)
 
+count = 0
 for v in initial:
-  blink(0,v)
+  count += blink(0,v)
 
-print(f"Answer: {count_stones[0]}")
+print(f"Answer: {count}")
