54cadcc64497d4c03ddc90aa9aba721af746c383
Alexandre Hagihara <alexandre.hagihara@gmail.com>
2024-12-13 12:42:00 -0300

    Using the improvement of part 2 on part 1

--- 11/part1.py
+++ 11/part1.py
@@ -5,19 +5,29 @@ input_file.close()
 
 initial = [int(v) for v in input_line.split()]
 
+cache_results = {}
+
 def blink(blink_times, value):
   if blink_times == 25:
     return 1
   
+  if (blink_times, value) in cache_results:
+    return cache_results[(blink_times, value)]
+  
   str_value = str(value)
   if value == 0:
-    return blink(blink_times+1, 1)
+    result = blink(blink_times+1, 1)
+    
   elif len(str_value) % 2 == 0:
     left = int(str_value[:len(str_value)//2])
     right = int(str_value[len(str_value)//2:])
-    return blink(blink_times+1, left) + blink(blink_times+1, right)
+    result = blink(blink_times+1, left) + blink(blink_times+1, right)
   else:
-    return blink(blink_times+1, value*2024)
+    result = blink(blink_times+1, value*2024)
+
+  cache_results[(blink_times, value)] = result
+  return result
+  
 
 count = 0
 for v in initial:
