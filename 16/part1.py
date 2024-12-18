0366292355740c6952206a326c2b6216dcfd1364
Alexandre Hagihara <alexandre.hagihara@gmail.com>
2024-12-18 13:05:23 -0300

    Refactoring graph survey to avoid recursion, adding some debug

--- 16/part1.py
+++ 16/part1.py
@@ -4,124 +4,73 @@ input_file = open("16/input.txt")
 input_lines = input_file.readlines()
 input_file.close()
 
-curr_r, curr_c = 0, 0
-end_r, end_c = 0, 0
-for r,line in enumerate(input_lines):
-  if 'E' in line:
-    end_r, end_c = r, line.index('E')  
-  elif 'S' in line:
-    curr_r, curr_c = r, line.index('S')
-
+start_node = (0,0)
+end_node = (0,0)
 graph = {}
-
-graph[(curr_r, curr_c)] = {}
 
 NORTH = 0
 EAST = 1
 SOUTH = 2
 WEST = 3
 
-# Our graph_survey is reaching python's default recursion limit of 1000
-import sys
-sys.setrecursionlimit(10000)
-
-# Create all graph connections
-def graph_survey(r, c):
-  # Get the current node
-  n = graph[(r, c)]
-
-  # If position at EAST is not a wall
-  if input_lines[r][c+1] != '#':
-    # Add position at EAST to current node
-    if (r,c+1) not in n:
-      n[(r,c+1)] = EAST
-    # If there's no node corresponding to this position on graph, create it and add current node to this new node
-    if (r,c+1) not in graph:
-      graph[(r,c+1)] = {(r,c):WEST}
-      graph_survey(r,c+1)
-    # If current position is not in the node, add it
-    elif (r,c) not in graph[(r,c+1)]:
-      graph[(r,c+1)][(r,c)] = WEST
-
-    
-  # If position at WEST is not a wall
-  if input_lines[r][c-1] != '#':
-    # Add position at WEST to current node
-    if (r,c-1) not in n:
-      n[(r,c-1)] = WEST
-    # If there's no node corresponding to this position on graph, create it and add current node to this new node
-    if (r,c-1) not in graph:
-      graph[(r,c-1)] = {(r,c):EAST}
-      graph_survey(r,c-1)
-    # If current position is not in the node, add it
-    elif (r,c) not in graph[(r,c-1)]:
-      graph[(r,c+1)][(r,c)] = EAST
-  
-  # If position at NORTH is not a wall
-  if input_lines[r-1][c] != '#':
-    # Add position at NORTH to current node
-    if (r-1,c) not in n:
-      n[(r-1,c)] = NORTH
-    # If there's no node corresponding to this position on graph, create it and add current node to this new node
-    if (r-1,c) not in graph:
-      graph[(r-1,c)] = {(r,c):SOUTH}
-      graph_survey(r-1,c)
-    # If current position is not in the node, add it
-    elif (r,c) not in graph[(r-1,c)]:
-      graph[(r-1,c)][(r,c)] = SOUTH
-  
-  # If position at SOUTH is not a wall
-  if input_lines[r+1][c] != '#':
-    # Add position at SOUTH to current node
-    if (r+1,c) not in n:
-      n[(r+1,c)] = SOUTH
-    # If there's no node corresponding to this position on graph, create it and add current node to this new node
-    if (r+1,c) not in graph:
-      graph[(r+1,c)] = {(r,c):NORTH}
-      graph_survey(r+1,c)
-    # If current position is not in the node, add it
-    elif (r,c) not in graph[(r+1,c)]:
-      graph[(r+1,c)][(r,c)] = NORTH
-
-# Create nodes
-graph_survey(curr_r, curr_c)
-
+# Create all graph nodes and map the start and end nodes
+for r, line in enumerate(input_lines):
+  for c, v in enumerate(line.replace('\n','')):
+    if v != '#':
+      node = {}
+      if input_lines[r+1][c] != '#':
+        node[(r+1,c)] = SOUTH
+      if input_lines[r][c-1] != '#':
+        node[(r,c-1)] = WEST
+      if input_lines[r][c+1] != '#':
+        node[(r,c+1)] = EAST
+      if input_lines[r-1][c] != '#':
+        node[(r-1,c)] = NORTH
+      graph[(r,c)] = node
+    if v == 'S':
+      start_node = (r, c)
+    elif v == 'E':
+      end_node = (r, c)
+        
 # for n in graph: print(n, graph[n])
 
 # Starting direction
 direction = EAST
 
 visited_list = []
-to_visit_list = [(curr_r, curr_c)]
+to_visit_list = [start_node]
 
 distances_list = {}
-distances_list[(curr_r, curr_c)] = (0, EAST)
+distances_list[start_node] = (0, EAST)
 for g in graph:
-  if g != (curr_r, curr_c):
+  if g != (start_node):
     distances_list[g] = (-1, -1)
 
 # print(distances_list)
 
-current = (curr_r, curr_c)
+curr_node = start_node
+
 # Djikstra
 while len(to_visit_list) > 0:
   nearest_dist = -1
   # First we search in the list of "to visit" for the nearest
   for n in to_visit_list:
     if nearest_dist == -1 or distances_list[n] < nearest_dist:
-      current = n
+      curr_node = n
       nearest_dist = distances_list[n]
 
-  neighbour_list = graph[current]
+  neighbour_list = graph[curr_node]
   for neighbour in neighbour_list:
-    # If they haven't been visited before, add them to the visit list
+    # If they haven't been visited before OR if we can give a better distance
     if neighbour not in visited_list:
+      # This can re-add an already visited node, because of the second condition above
       to_visit_list.append(neighbour)
 
       # Retrieving current node least distance and best direction
-      curr_dist, curr_dir = distances_list[current]
+      curr_dist, curr_dir = distances_list[curr_node]
       # Retrieving neighbour direction relative to current node
       neighbour_dir = neighbour_list[neighbour]
+
       # If we have the same direction, the new distance is just one
       if curr_dir == neighbour_dir:
         new_dist = curr_dist + 1
@@ -132,15 +81,62 @@ while len(to_visit_list) > 0:
       # Get the current distance of neighbour
       neigh_dist, _ = distances_list[neighbour]
       # If the new distance is shorter, use this new distance (and update the best distance)
-      if neigh_dist == -1 or new_dist < neigh_dist:
+      if neigh_dist == -1 or new_dist <= neigh_dist:
         distances_list[neighbour] = (new_dist, neighbour_dir)
+        
+
+
   # Moving current node to "visited" list
-  to_visit_list.remove(current)
-  visited_list.append(current)
+  to_visit_list.remove(curr_node)
+  if curr_node not in visited_list:
+    visited_list.append(curr_node)
 
-print(distances_list)
+# for d in graph: print(d, distances_list[d])
 
-print(f"Answer: {distances_list[end_r, end_c][0]}")
+print(f"Answer: {distances_list[end_node][0]}")
+
+debug_map = []
+for line in input_lines:
+  debug_line = []
+  for c in line.replace('\n','').replace('.', ' '):
+    debug_line.append(c)
+  debug_map.append(debug_line)
+
+curr_node = end_node
+while( curr_node != start_node ):
+  d = curr_node
+  if distances_list[d][1] == NORTH:
+    debug_map[d[0]][d[1]] = '@'
+    curr_node = (d[0]+1, d[1])
+  elif distances_list[d][1] == SOUTH:
+    debug_map[d[0]][d[1]] = '@'
+    curr_node = (d[0]-1, d[1])
+  elif distances_list[d][1] == EAST:
+    debug_map[d[0]][d[1]] = '@'
+    curr_node = (d[0], d[1]-1)
+  elif distances_list[d][1] == WEST:
+    debug_map[d[0]][d[1]] = '@'
+    curr_node = (d[0], d[1]+1)
+  
+# for line in debug_map:
+#   print(''.join(line))
+
+for d in distances_list:
+  if distances_list[d][1] == NORTH:
+    debug_map[d[0]][d[1]] = '^'
+  elif distances_list[d][1] == SOUTH:
+    debug_map[d[0]][d[1]] = 'v'
+  elif distances_list[d][1] == EAST:
+    debug_map[d[0]][d[1]] = '>'
+  elif distances_list[d][1] == WEST:
+    debug_map[d[0]][d[1]] = '<'
+
+# for line in debug_map:
+#   print(''.join(line))
 
 # 134596: too high
 # TODO: use the distances_list to backtrace the shortest path
+# The shortest path discounting the 90 degree cost is 504
+# Using friend's code, the result was 134588, let's try to reach this magic value
+# My mistake: compute the distance including the 90 degree cost, but we can't include it yet
+# Maybe the best approach is calculate every path and then choose the shortest
