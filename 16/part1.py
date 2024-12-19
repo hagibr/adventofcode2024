c775b126cb31f3634536494ccf2eee2b50d7bc09
Alexandre Hagihara <alexandre.hagihara@gmail.com>
2024-12-19 10:14:57 -0300

    Day 16 part 1 is working

--- 16/part1.py
+++ 16/part1.py
@@ -1,12 +1,12 @@
-# input_file = open("16/example.txt")
-# input_file = open("16/example2.txt")
-input_file = open("16/input.txt")
+# input_file = open("16/example.txt") # Expected: 7036
+# input_file = open("16/example2.txt") # Expected: 11048
+input_file = open("16/input.txt") # Expected: 134588 with my input
 input_lines = input_file.readlines()
 input_file.close()
 
 start_node = (0,0)
 end_node = (0,0)
-graph = {}
+graph = dict()
 
 NORTH = 0
 EAST = 1
@@ -15,128 +15,112 @@ WEST = 3
 
 # Create all graph nodes and map the start and end nodes
 for r, line in enumerate(input_lines):
-  for c, v in enumerate(line.replace('\n','')):
+  for c, v in enumerate(line.strip()):
     if v != '#':
-      node = {}
-      if input_lines[r+1][c] != '#':
-        node[(r+1,c)] = SOUTH
-      if input_lines[r][c-1] != '#':
-        node[(r,c-1)] = WEST
-      if input_lines[r][c+1] != '#':
-        node[(r,c+1)] = EAST
+      # Every coordinate generate 4 nodes
+      node_north = dict()
+      node_east = dict()
+      node_south = dict()
+      node_west = dict()
+      
+      # If we can go north
       if input_lines[r-1][c] != '#':
-        node[(r-1,c)] = NORTH
-      graph[(r,c)] = node
+        # Add the north position to north node
+        node_north[(r-1,c,NORTH)] = 1
+        # Add the possibility to turn to north from east and west node
+        node_east[(r,c,NORTH)] = 1000
+        node_west[(r,c,NORTH)] = 1000
+      # If we can go east
+      if input_lines[r][c+1] != '#':
+        # Add the east position to east node
+        node_east[(r,c+1,EAST)] = 1
+        # Add the possibility to turn to east from north and south node
+        node_north[(r,c,EAST)] = 1000
+        node_south[(r,c,EAST)] = 1000
+      # If we can go south  
+      if input_lines[r+1][c] != '#':
+        # Add the north position to south node
+        node_south[(r+1,c,SOUTH)] = 1
+        # Add the possibility to turn to south from east and west node
+        node_east[(r,c,SOUTH)] = 1000
+        node_west[(r,c,SOUTH)] = 1000
+      # If we can go west
+      if input_lines[r][c-1] != '#':
+        # Add the west position to west node
+        node_west[(r,c-1,WEST)] = 1
+        # Add the possibility to turn to west from north and south node
+        node_north[(r,c,WEST)] = 1000
+        node_south[(r,c,WEST)] = 1000
+      
+      # Adding the nodes to graph (even the empty ones)
+      graph[(r,c,NORTH)] = node_north
+      graph[(r,c,EAST)] = node_east
+      graph[(r,c,SOUTH)] = node_south
+      graph[(r,c,WEST)] = node_west
+
+    # We always start pointed to EAST  
     if v == 'S':
-      start_node = (r, c)
+      start_node = (r,c,EAST)
+    # Doesn't matter here which direction, including all
     elif v == 'E':
-      end_node = (r, c)
+      end_node = [(r,c,NORTH),(r,c,EAST),(r,c,SOUTH),(r,c,WEST)]
         
 # for n in graph: print(n, graph[n])
 
-# Starting direction
-direction = EAST
-
+# These lists are used on Djikstra's algorithm
 visited_list = []
 to_visit_list = [start_node]
 
-distances_list = {}
-distances_list[start_node] = (0, EAST)
+# Initializing the costs of each node (even the empty ones)
+cost_list = {}
 for g in graph:
-  if g != (start_node):
-    distances_list[g] = (-1, -1)
+  cost_list[g] = -1 # -1 means infinity
+# The start node has cost 0
+cost_list[start_node] = 0
 
 # print(distances_list)
 
-curr_node = start_node
-
 # Djikstra
 while len(to_visit_list) > 0:
-  nearest_dist = -1
-  # First we search in the list of "to visit" for the nearest
-  for n in to_visit_list:
-    if nearest_dist == -1 or distances_list[n] < nearest_dist:
+  # First we search in the list of "to visit" for the one with lowest cost
+  curr_node = to_visit_list[0]
+  curr_cost = cost_list[curr_node]
+  for n in to_visit_list[1:]:
+    if cost_list[n] < curr_cost:
       curr_node = n
-      nearest_dist = distances_list[n]
+      curr_cost = cost_list[n]
 
-  neighbour_list = graph[curr_node]
-  for neighbour in neighbour_list:
-    # If they haven't been visited before OR if we can give a better distance
-    if neighbour not in visited_list:
-      # This can re-add an already visited node, because of the second condition above
+  # Get the list of neighbour nodes.
+  # The key is the node coordinates (row, column, direction) and the value is the cost
+  neighbour_nodes_cost = graph[curr_node]
+
+  # For each neighbour...
+  for neighbour in neighbour_nodes_cost:
+    # Ignore if this neighbour was already visited
+    if neighbour in visited_list:
+      continue
+
+    # Get the current distance of the neighbour
+    old_distance = cost_list[neighbour]
+
+    # Calculating the new distance if we go from current node
+    new_distance = curr_cost + neighbour_nodes_cost[neighbour]
+
+    # Updating the distance
+    if old_distance == -1 or new_distance < old_distance:
+      cost_list[neighbour] = new_distance
+    
+    # Adding the node to the "to visit list"
+    if neighbour not in end_node and neighbour not in to_visit_list:
       to_visit_list.append(neighbour)
-
-      # Retrieving current node least distance and best direction
-      curr_dist, curr_dir = distances_list[curr_node]
-      # Retrieving neighbour direction relative to current node
-      neighbour_dir = neighbour_list[neighbour]
-
-      # If we have the same direction, the new distance is just one
-      if curr_dir == neighbour_dir:
-        new_dist = curr_dist + 1
-      # If we have to change the direction, the new distance is incremented by 1000
-      else:
-        new_dist = curr_dist + 1001
-      
-      # Get the current distance of neighbour
-      neigh_dist, _ = distances_list[neighbour]
-      # If the new distance is shorter, use this new distance (and update the best distance)
-      if neigh_dist == -1 or new_dist <= neigh_dist:
-        distances_list[neighbour] = (new_dist, neighbour_dir)
-        
-
-
+    
   # Moving current node to "visited" list
   to_visit_list.remove(curr_node)
-  if curr_node not in visited_list:
-    visited_list.append(curr_node)
+  visited_list.append(curr_node)
 
 # for d in graph: print(d, distances_list[d])
 
-print(f"Answer: {distances_list[end_node][0]}")
-
-debug_map = []
-for line in input_lines:
-  debug_line = []
-  for c in line.replace('\n','').replace('.', ' '):
-    debug_line.append(c)
-  debug_map.append(debug_line)
-
-curr_node = end_node
-while( curr_node != start_node ):
-  d = curr_node
-  if distances_list[d][1] == NORTH:
-    debug_map[d[0]][d[1]] = '@'
-    curr_node = (d[0]+1, d[1])
-  elif distances_list[d][1] == SOUTH:
-    debug_map[d[0]][d[1]] = '@'
-    curr_node = (d[0]-1, d[1])
-  elif distances_list[d][1] == EAST:
-    debug_map[d[0]][d[1]] = '@'
-    curr_node = (d[0], d[1]-1)
-  elif distances_list[d][1] == WEST:
-    debug_map[d[0]][d[1]] = '@'
-    curr_node = (d[0], d[1]+1)
-  
-# for line in debug_map:
-#   print(''.join(line))
-
-for d in distances_list:
-  if distances_list[d][1] == NORTH:
-    debug_map[d[0]][d[1]] = '^'
-  elif distances_list[d][1] == SOUTH:
-    debug_map[d[0]][d[1]] = 'v'
-  elif distances_list[d][1] == EAST:
-    debug_map[d[0]][d[1]] = '>'
-  elif distances_list[d][1] == WEST:
-    debug_map[d[0]][d[1]] = '<'
-
-# for line in debug_map:
-#   print(''.join(line))
-
-# 134596: too high
-# TODO: use the distances_list to backtrace the shortest path
-# The shortest path discounting the 90 degree cost is 504
-# Using friend's code, the result was 134588, let's try to reach this magic value
-# My mistake: compute the distance including the 90 degree cost, but we can't include it yet
-# Maybe the best approach is calculate every path and then choose the shortest
+# We have 4 nodes for end coordinates.
+# Print them all and select the one with lowest cost, it will reflect the direction where it came from.
+for end in end_node:
+  print(f"Answer: {cost_list[end]} ({end})")
