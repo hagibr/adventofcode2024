92b08c2c4c4562f00be8f9b3935c5227619e943e
Alexandre Hagihara <alexandre.hagihara@gmail.com>
2024-12-20 12:04:30 -0300

    New approach that solved Day 17 part 2

--- 17/part2.py
+++ 17/part2.py
@@ -18,69 +18,105 @@ for line in input_lines:
 
 # print(A,B,C,Program)
 
-def run_program(value_A, value_B, value_C):
-  A, B, C = value_A, value_B, value_C
+# Analysis:
+# My input program is 2,4,1,1,7,5,4,7,1,4,0,3,5,5,3,0
+# Let's decompile it (8 instructions):
+# 1st: 2,4 -> bst cmb_A [B = A % 8]
+# 2nd: 1,1 -> bxl lit_1 [B = B ^ 1]
+# 3rd: 7,5 -> cdv cmb_A [C = A // 2**B]
+# 4th: 4,7 -> bxc       [B = B ^ C]
+# 5th: 1,4 -> bxl lit_4 [B = B ^ 4]
+# 6th: 0,3 -> adv cmb_3 [A = A // 2**3]
+# 7th: 5,5 -> out B     [outputs current value at B modulo 8]
+# 8th: 3,0 -> jnz 0     [if A not 0, jump to 1st instruction]
+
+# Let's translate them into Python code
+def run_program(value_A):
+  # Initialization
+  A = value_A
+  B = C = 0
   out = []
-  # Let's process
-  ip = 0
-  while(ip < len(Program)):
-    opcode = Program[ip]
-    literal = Program[ip+1]
-    combo = 0
-    if 0 <= literal <= 3:
-      combo = literal
-    elif literal == 4:
-      combo = A
-    elif literal == 5:
-      combo = B
-    elif literal == 6:
-      combo = C
-    
-    # adv
-    if opcode == 0:
-      A = A >> combo
-    # bxl
-    elif opcode == 1:
-      B = B ^ literal
-    # bst
-    elif opcode == 2:
-      B = combo % 8
-    # jnz
-    elif opcode == 3:
-      if A != 0:
-        ip = literal
-        continue
-    # bxc
-    elif opcode == 4:
-      B = B ^ C
-    # out
-    elif opcode == 5:
-      out.append(combo % 8)
-      # Speeding a bit the analysis by comparing the current output with the reference program
-      if Program[:len(out)] != out:
-        return out # Early exit
-      
-    # bdv
-    elif opcode == 6:
-      B = A >> combo
-    # cdv
-    elif opcode == 7:
-      C = A >> combo
-    # invalid???
-    else:
-      print(f"Invalid opcode: {opcode}")
-    # next instruction...
-    ip += 2
+  # Loop
+  while (A != 0):     # jnz 0
+    B = A % 8         # bst cmb_A
+    B = B ^ 1         # bxl lit_1
+    C = A >> B        # cdv cmb_A
+    B = B ^ C         # bxc
+    B = B ^ 4         # bxl lit_4
+    A = A // 8        # adv cmb_3
+    out.append(B % 8) # out B
+    # This is an optimization for our implementation: exiting when we know that we already know that A is not right
+    if( out != Program[:len(out)]) : break
   return out
   
-print(Program)
-#print(run_program(corrupted_A,initial_B,initial_C))
-#print(run_program(117440,initial_B,initial_C))
+# I've tried to make sense by reversing the operations, without success. So I've changed my approach.
 
-A = 0
-while(True):
-  if run_program(A,initial_B,initial_C) == Program:
-    break
-  A += 1
+# Iterative solution. As we saw that we shift A by 3 bits on every loop, and we want 16 outputted values,
+# Then the lowest candidate is 8**(16-1) and it should be lower than 8**16
 
-print(f"Answer: {A}")
+# For our iteration, let's implement a helper function
+def iterator(offset, increment):
+  value_A = 8**15 + offset # This is the initial value. Minimum of 8**15 to have 16 outputted values
+  count = 0
+  while(value_A < 8**16): # Maximum of 8**16, because it would give us 17 outputted values
+    out = run_program(value_A)
+    # Debugging the evolution of the number of outputted values
+    if( len(out) > count or out == Program[:len(out)]):
+      count = len(out)
+      # Printing the decimal value, binary value, outputted value and number of elements of output
+      print(value_A, bin(value_A), out, count)
+      if( out == Program ):
+        break
+    value_A += increment
+  return value_A # Only when we found the value
+
+# This is the first loop that I've tried. Incrementing one by one.
+# answer = iterator(0,1)
+
+# This loop gave me these 5 outputs at the first 30 seconds
+# 35184372088832 0b1000000000000000000000000000000000000000000000 [5] 1
+# 35184372088839 0b1000000000000000000000000000000000000000000111 [2, 5] 2
+# 35184372089043 0b1000000000000000000000000000000000000011010011 [2, 4, 6] 3
+# 35184372089386 0b1000000000000000000000000000000000001000101010 [2, 4, 1, 5] 4
+# 35184372099626 0b1000000000000000000000000000000010101000101010 [2, 4, 1, 1, 7, 5, 5] 7
+
+# Now it's time to do some inference here. It seems that the last 6 bits must be be 0b101010
+# So now we increment by 0b1000000
+# answer = iterator( 0b101010,
+#                   0b1000000)
+
+# OK, some progress. Now the output is:
+# 35184372088874 0b1000000000000000000000000000000000000000101010 [2, 0] 2
+# 35184372089386 0b1000000000000000000000000000000000001000101010 [2, 4, 1, 5] 4
+# 35184372099626 0b1000000000000000000000000000000010101000101010 [2, 4, 1, 1, 7, 5, 5] 7
+# 35184378915370 0b1000000000000000000000011010000010101000101010 [2, 4, 1, 1, 7, 5, 4, 6] 8
+# 35184618514986 0b1000000000000000001110101100000010101000101010 [2, 4, 1, 1, 7, 5, 4, 7, 3] 9
+
+# Now the last bits are 0b10101000101010, so we increment by 0b100000000000000
+# answer = iterator( 0b10101000101010,
+#                   0b100000000000000)
+
+# 35184372099626 0b1000000000000000000000000000000010101000101010 [2, 4, 1, 1, 7, 5, 5] 7
+# 35184378915370 0b1000000000000000000000011010000010101000101010 [2, 4, 1, 1, 7, 5, 4, 6] 8
+# 35184618514986 0b1000000000000000001110101100000010101000101010 [2, 4, 1, 1, 7, 5, 4, 7, 3] 9
+# 35187857041962 0b1000000000000011001111101110000010101000101010 [2, 4, 1, 1, 7, 5, 4, 7, 1, 5] 10
+# 35190255659562 0b1000000000000101011110101100000010101000101010 [2, 4, 1, 1, 7, 5, 4, 7, 1, 4, 0, 5] 1
+
+# There are some problems here, so let's be conservative and keep just the last bits and change the increment
+# answer = iterator(      0b10101000101010,
+#                   0b10000000000000000000)
+
+# 35184372099626 0b1000000000000000000000000000000010101000101010 [2, 4, 1, 1, 7, 5, 5] 7
+# 35184378915370 0b1000000000000000000000011010000010101000101010 [2, 4, 1, 1, 7, 5, 4, 6] 8
+# 35184618514986 0b1000000000000000001110101100000010101000101010 [2, 4, 1, 1, 7, 5, 4, 7, 3] 9
+# 35187857041962 0b1000000000000011001111101110000010101000101010 [2, 4, 1, 1, 7, 5, 4, 7, 1, 5] 10
+# 35190255659562 0b1000000000000101011110101100000010101000101010 [2, 4, 1, 1, 7, 5, 4, 7, 1, 4, 0, 5] 12
+# 35241258396202 0b1000000000110100111110101100000010101000101010 [2, 4, 1, 1, 7, 5, 4, 7, 1, 4, 0, 3, 5, 5, 1] 15
+
+# ALmost there, it seems that the iteration with output size 10 was the wrong one. Let's use the matching
+# parts of size 12 and size 15
+answer = iterator( 0b11110101100000010101000101010,
+                  0b100000000000000000000000000000)
+
+
+print(f"Answer: {answer}")
